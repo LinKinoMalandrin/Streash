3422f1a51c5c34d9b7cd00df18269affa7f283af
BuzzerM <melissa.buczko@gmail.com>
2018-12-22 22:46:09 +0100

    commentaire Charchain

--- src/streash/vars/CharChain.java
+++ src/streash/vars/CharChain.java
@@ -2,28 +2,30 @@ package streash.vars;
 
 import java.util.Scanner;
 
-public class CharChain implements Value{
+public class CharChain implements Value {
 	private String value;
-	
+
 	public CharChain(String value) {
 		this.value = value;
 	}
-	
+
 	public String toString() {
 		return value;
 	}
-	public String getConsoleString(){
-		return "\""+value+"\"";
+
+	public String getConsoleString() {
+		return "\"" + value + "\"";
 	}
-	
+
 	@Override
 	public String getType() {
 		return "String";
 	}
-	
+
 	public CharChain concat(CharChain c) {
 		return new CharChain(value.concat(c.value));
 	}
+
 	@Override
 	public int compareTo(Object arg0) {
 		if (arg0 instanceof Number)
@@ -31,6 +33,15 @@ public class CharChain implements Value{
 		CharChain c = (CharChain) arg0;
 		return c.value.compareTo(value);
 	}
+
+	/**
+	 * Multiply this object's value by the number indicated in the argument
+	 * 
+	 * @param i
+	 *            the number of time the value is going to be multiplicated
+	 * @return a new Charchain that represents this object's value multiplied i
+	 *         times
+	 */
 	public CharChain time(long i) {
 		if (i < 0)
 			throw new IllegalArgumentException("Cannot time a String by a negative number");
@@ -40,41 +51,66 @@ public class CharChain implements Value{
 
 		return new CharChain(builder.toString());
 	}
-	
 
+	/**
+	 * Remove the Charchain passed in argument from the end of this object's value
+	 * if the argument is a suffix of the value
+	 * 
+	 * @param c
+	 *            the Charchain that will be removed
+	 * @return a new Charchain that represent this object's value without the
+	 *         argument at the end
+	 */
 	public CharChain subSuffix(CharChain c) {
 		String string = c.value;
 		if (!isSuffix(string, value))
-			throw new IllegalArgumentException(string+" is not a suffix of "+value);
-		return new CharChain(value.substring(0, value.length()-string.length()));
+			throw new IllegalArgumentException(string + " is not a suffix of " + value);
+		return new CharChain(value.substring(0, value.length() - string.length()));
 	}
 
-	private boolean isSuffix(String sub, String string) 
-	{ 
-	    int n1 = sub.length(), n2 = string.length(); 
-	    if (n1 > n2) 
-	      return false; 
-	    for (int i=0; i < n1; i++) 
-	       if (sub.charAt(n1 - i - 1) != string.charAt(n2 - i - 1)) 
-	           return false; 
-	    return true; 
-	} 
-	
+	/**
+	 * 
+	 * @param sub
+	 *            the suffix
+	 * @param string
+	 *            the Charchain
+	 * @return true if the sub is a suffix of string. False otherwise.
+	 */
+	private boolean isSuffix(String sub, String string) {
+		int n1 = sub.length(), n2 = string.length();
+		if (n1 > n2)
+			return false;
+		for (int i = 0; i < n1; i++)
+			if (sub.charAt(n1 - i - 1) != string.charAt(n2 - i - 1))
+				return false;
+		return true;
+	}
+
 	public static CharChain parse(String beginning, Scanner s) {
 		StringBuilder to = new StringBuilder();
 		boolean end = false;
 		String part = beginning.substring(1);
-		while(true) {
+		while (true) {
 			if (!(part.contains("\"")))
 				to.append(part);
 			else {
 				for (char c : part.toCharArray()) {
-					if (c == '"' && end) { end = false; to.append('"'); continue; }
-					if (end && c != '"') { throw new IllegalStateException("Wrong declaration of string var"); }
-					if (c == '"') { end = true; continue; }
+					if (c == '"' && end) {
+						end = false;
+						to.append('"');
+						continue;
+					}
+					if (end && c != '"') {
+						throw new IllegalStateException("Wrong declaration of string var");
+					}
+					if (c == '"') {
+						end = true;
+						continue;
+					}
 					to.append(c);
 				}
-				if (end) return new CharChain(to.toString());
+				if (end)
+					return new CharChain(to.toString());
 			}
 			if (!s.hasNext())
 				throw new IllegalStateException("Missing closure of string declaration");
@@ -82,33 +118,33 @@ public class CharChain implements Value{
 			part = s.next();
 		}
 	}
-	
+
 	@Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((value == null) ? 0 : value.hashCode());
-        return result;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        CharChain other = (CharChain) obj;
-        if (value == null) {
-            if (other.value != null)
-                return false;
-        } else if (!value.equals(other.value))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CharChain other = (CharChain) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println(CharChain.parse("\"armand", s));
 	}
--- src/streash/vars/Number.java
+++ src/streash/vars/Number.java
@@ -3,7 +3,7 @@ package streash.vars;
 public class Number implements Value {
 	private long num;
 	private long den;
-	
+
 	public Number(long num, long den) {
 		this.num = num;
 		this.den = den;
@@ -12,10 +12,12 @@ public class Number implements Value {
 		else
 			this.proper();
 	}
+
 	public Number(long value) {
 		this.num = value;
 		this.den = 1;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Number))
@@ -23,43 +25,49 @@ public class Number implements Value {
 		Number n = (Number) obj;
 		return n.num == num && n.den == den;
 	}
+
 	@Override
 	public int compareTo(Object arg0) {
 		if (arg0 instanceof CharChain)
 			return -1;
 		Number n = (Number) arg0;
-		return Float.valueOf((float) num / den).compareTo(Float.valueOf((float) n.num/n.den));
+		return Float.valueOf((float) num / den).compareTo(Float.valueOf((float) n.num / n.den));
 	}
-	
+
 	@Override
 	public String toString() {
 		if (den == 1)
 			return String.valueOf(num);
 		if (den == -1)
 			return String.valueOf(-num);
-		return String.valueOf(num)+"/"+String.valueOf(den)+" ~= "+String.valueOf((float) num/den);
+		return String.valueOf(num) + "/" + String.valueOf(den) + " ~= " + String.valueOf((float) num / den);
 	}
+
 	public String getConsoleString() {
 		return this.toString();
 	}
+
 	@Override
 	public String getType() {
 		return "Number";
 	}
+
 	public long getValue() {
 		if (den != 1)
 			throw new IllegalStateException("Cannot give value of floating Number");
 		return num;
 	}
+
 	public float getFloatingValue() {
 		return (float) num / den;
 	}
+
 	public static Number requireNonFloat(Number n, String message) {
-		if(!n.isInteger())
+		if (!n.isInteger())
 			throw new IllegalArgumentException(message);
 		return n;
 	}
-	
+
 	public static Number parse(String expr) {
 		String[] split = expr.split("/");
 		if (split.length > 2)
@@ -68,7 +76,7 @@ public class Number implements Value {
 			return new Number(Integer.parseInt(expr));
 		return new Number(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
 	}
-	
+
 	private Number proper() {
 		if (den == num) {
 			num = 1;
@@ -82,13 +90,18 @@ public class Number implements Value {
 			return this;
 		}
 		long pgcd = pgcd(den, num);
-		this.num = num/pgcd;
-		this.den = den/pgcd;
+		this.num = num / pgcd;
+		this.den = den / pgcd;
 		return this;
 	}
+
 	private long pgcd(long a, long b) {
 		long c;
-		if (a < b) { c = a; a = b; b = c; }
+		if (a < b) {
+			c = a;
+			a = b;
+			b = c;
+		}
 
 		while (a % b != 0) {
 			c = a % b;
@@ -97,34 +110,38 @@ public class Number implements Value {
 		}
 		return b;
 	}
+
 	public Number add(Number n) {
 		if (n.equals(new Number(0)))
 			return this;
 		if (this.equals(new Number(0)))
 			return n;
-		
-		return new Number(num*n.den + n.num*den, den*n.den).proper();
+
+		return new Number(num * n.den + n.num * den, den * n.den).proper();
 	}
-	
+
 	public Number sub(Number n) {
 		if (n.equals(new Number(0)))
 			return this;
 		if (this.equals(new Number(0)))
 			return n.mul(new Number(-1));
-		return new Number(num*n.den - n.num*den, den*n.den).proper();
+		return new Number(num * n.den - n.num * den, den * n.den).proper();
 	}
+
 	public Number mul(Number n) {
-		return new Number(num*n.num, den*n.den).proper();
+		return new Number(num * n.num, den * n.den).proper();
 	}
+
 	public Number div(Number n) {
 		if (n.equals(new Number(0)))
 			throw new IllegalArgumentException("Cannot divide a Number by 0");
-		return new Number(num*n.den, den*n.num).proper();
+		return new Number(num * n.den, den * n.num).proper();
 	}
-	
+
 	public boolean isInteger() {
 		return den == 1;
 	}
+
 	public long getNum() {
 		return num;
 	}
